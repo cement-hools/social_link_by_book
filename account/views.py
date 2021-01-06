ddc5d39db23e46665c1b82708092346b9edafc02
Максим Секачёв <cement-fan@ya.ru>
2021-01-06 11:12:34 +0300

    readme

--- account/views.py
+++ account/views.py
@@ -34,25 +34,27 @@ def user_login(request):
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
 
+
 def register(request):
-        if request.method == 'POST':
-            user_form = UserRegistrationForm(
-                request.POST)  # UserCreationForm custom form
-            if user_form.is_valid():
-                # Создаем нового пользователя, но пока не сохраняем в базу данных.
-                new_user = user_form.save(commit=False)
-                # Задаем пользователю зашифрованный пароль.
-                new_user.set_password(user_form.cleaned_data['password'])
-                # Сохраняем пользователя в базе данных.
-                # Создание профиля пользователя.
-                Profile.objects.create(user=new_user)
-                new_user.save()
-                return render(request, 'account/register_done.html',
-                              {'new_user': new_user})
-        else:
-            user_form = UserRegistrationForm()
-        return render(request, 'account/register.html',
-                      {'user_form': user_form})
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(
+            request.POST)  # UserCreationForm custom form
+        if user_form.is_valid():
+            # Создаем нового пользователя, но пока не сохраняем в базу данных.
+            new_user = user_form.save(commit=False)
+            # Задаем пользователю зашифрованный пароль.
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Сохраняем пользователя в базе данных.
+            # Создание профиля пользователя.
+            Profile.objects.create(user=new_user)
+            new_user.save()
+            return render(request, 'account/register_done.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html',
+                  {'user_form': user_form})
+
 
 @login_required
 def edit(request):
